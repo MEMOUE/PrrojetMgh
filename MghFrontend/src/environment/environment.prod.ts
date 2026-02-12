58564a1e9fb7fe9973f0172da31faf139ce4883d
Memko <memko021@gmail.com>
2026-02-12 08:07:54 +0000

    fix:mise a jour des confs docker et agnix

--- MghFrontend/src/environment/environment.prod.ts
+++ MghFrontend/src/environment/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   //apiUrl: 'http://localhost:8080/api',
   //fileUploadUrl: 'http://localhost:8080/api/uploads',
 
-  apiUrl: 'https://api.iub-university.com/api',
-  fileUploadUrl: 'https://api.iub-university.com/api/uploads'
-};
+  apiUrl: 'https://api.magestionhotel.com/api',
+  fileUploadUrl: 'https://api.magestionhotel.com/api/uploads'
+};  
--- MghFrontend/src/environment/environment.ts
+++ MghFrontend/src/environment/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   //apiUrl: 'http://localhost:8080/api',
   //fileUploadUrl: 'http://localhost:8080/api/uploads',
 
-  apiUrl: 'https://api.iub-university.com/api',
-  fileUploadUrl: 'https://api.iub-university.com/api/uploads'
+  apiUrl: 'https://api.magestionhotel.com/api',
+  fileUploadUrl: 'https://api.magestionhotel.com/api/uploads'
 };
 
